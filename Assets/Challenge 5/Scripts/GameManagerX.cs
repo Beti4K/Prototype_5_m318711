472cf3397df6689081e4d313898681ead37c487b
Beti4K <beata.kolodziejczyk2004@gmail.com>
2024-05-13 15:50:23 +0100

    Fixing bugs
    Completing challenge 5 - fixing bugs

--- Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -15,7 +15,7 @@ public class GameManagerX : MonoBehaviour
     public List<GameObject> targetPrefabs;
 
     private int score;
-    private float spawnRate = 1.5f;
+    private float spawnRate = 6f;
     public bool isGameActive;
 
     private float spaceBetweenSquares = 2.5f; 
@@ -23,13 +23,14 @@ public class GameManagerX : MonoBehaviour
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
         spawnRate /= 5;
         isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+        spawnRate /= difficulty;
         titleScreen.SetActive(false);
     }
 
@@ -70,14 +71,14 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        scoreText.text = "score: " + score;
     }
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
     }
 
